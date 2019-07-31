aa85e74c944f4d8f23a9a69ff1de0cb231ef89f5
Chris Wells <wellscre@yahoo.com>
2019-07-30 20:11:58 -0400

    added color randomization - lerp_lines

--- p5.js/lerp_lines.js
+++ p5.js/lerp_lines.js
@@ -3,10 +3,15 @@ var y = 0;
 var x1, y1;
 var lerper;
 var count = 0;
+var r,g,b,gray;
 
 function setup() {
   createCanvas(400, 400);
-  //background(100,0,0);
+  background(random(256),random(256),random(256));
+  r = random(256);
+  g = random(256);
+  b = random(256);
+  gray = random(256);
 }
 
 function draw() {
@@ -17,7 +22,7 @@ function draw() {
 function mousePressed() {
   for (var j = 0;j<1000;j++){
   if (count % 2 == 0) {
-    stroke(127);
+    stroke(gray);
     for (var i = 0; i<15; i++) {
       x1 = random(100)-15 + x;
       y1 = random(100)-15 + y;
@@ -28,7 +33,7 @@ function mousePressed() {
     } 
     x = 400;
   } else { 
-    stroke(0,0,0);
+    stroke(r,g,b);
     for (var i = 0; i<5; i++) {
       x1 = x - random(100)-15;
       y1 = random(180)-15 + y;
